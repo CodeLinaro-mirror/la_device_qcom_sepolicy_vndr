b786f4ce10305e373733faa40c9256a5cb79a6ec
Himanshu Agrawal <quic_hagraw@quicinc.com>
2022-01-18 20:33:07 +0530

    sepolicy_vndr: Initial changes for Atherton Split
    Change-Id: Id3bb6c9f7505c172c4f756963dceb89ab44fd197

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -1,79 +1,61 @@
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
-SEPOLICY_PATH:= $(BOARD_SEPOLICY_DIR)
+SEPOLICY_PATH:= device/qcom/sepolicy_vndr
+QSSI_SEPOLICY_PATH:= device/qcom/sepolicy
+SYS_ATTR_PROJECT_PATH := $(TOP)/device/qcom/sepolicy/generic/public/attribute
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(SEPOLICY_PATH)/generic/public \
-    $(SEPOLICY_PATH)/generic/public/attribute
+    $(QSSI_SEPOLICY_PATH)/generic/public \
+    $(QSSI_SEPOLICY_PATH)/generic/public/attribute
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
     $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(SEPOLICY_PATH)/generic/private
+    $(QSSI_SEPOLICY_PATH)/generic/private
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(SEPOLICY_PATH)/qva/public \
-    $(SEPOLICY_PATH)/qva/public/attribute
+    $(QSSI_SEPOLICY_PATH)/qva/public \
+    $(QSSI_SEPOLICY_PATH)/qva/public/attribute
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
     $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(SEPOLICY_PATH)/qva/private
+    $(QSSI_SEPOLICY_PATH)/qva/private
 
 #once all the services are moved to Product /ODM above lines will be removed.
 # sepolicy rules for product images
 PRODUCT_PUBLIC_SEPOLICY_DIRS := \
     $(PRODUCT_PUBLIC_SEPOLICY_DIRS) \
-    $(SEPOLICY_PATH)/generic/product/public \
-    $(SEPOLICY_PATH)/qva/product/public 
-
+    $(QSSI_SEPOLICY_PATH)/generic/product/public \
+    $(QSSI_SEPOLICY_PATH)/qva/product/public
 PRODUCT_PRIVATE_SEPOLICY_DIRS := \
     $(PRODUCT_PRIVATE_SEPOLICY_DIRS) \
-    $(SEPOLICY_PATH)/generic/product/private \
-    $(SEPOLICY_PATH)/qva/product/private
+    $(QSSI_SEPOLICY_PATH)/generic/product/private \
+    $(QSSI_SEPOLICY_PATH)/qva/product/private
 
-ifeq (,$(filter sdm845 sdm710 sdm660 msm8953 msm8998, $(TARGET_BOARD_PLATFORM)))
-    BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
-       $(SEPOLICY_PATH) \
-       $(SEPOLICY_PATH)/generic/vendor/common \
-       $(SEPOLICY_PATH)/qva/vendor/common/sysmonapp \
-       $(SEPOLICY_PATH)/qva/vendor/ssg \
-       $(SEPOLICY_PATH)/qva/vendor/common
+BOARD_SEPOLICY_DIRS := \
+   $(BOARD_SEPOLICY_DIRS) \
+   $(SEPOLICY_PATH) \
+   $(SEPOLICY_PATH)/generic/vendor/common \
+   $(SEPOLICY_PATH)/generic/vendor/common/attribute \
+   $(SEPOLICY_PATH)/qva/vendor/ssg \
+   $(SEPOLICY_PATH)/qva/vendor/common
 
-    ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM)
-      BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM)
-    else
-      BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR)
-      BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
-    endif
-    ifeq ($(TARGET_SUPPORTS_ANDROID_WEAR),true)
-      BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/sdm429w/
-    else
-      BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/sdm429w_law
-    endif
+  ifeq ($(TARGET_SEPOLICY_DIR),)
+    BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM)
+    BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM)
+  else
+    BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR)
+    BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
+  endif
+  ifeq ($(TARGET_SUPPORTS_ANDROID_WEAR),true)
+    BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/sdm429w/
+  else
+    BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/sdm429w_law
+  endif
 
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/test
-    BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test
-    endif
-endif
-
-ifneq (,$(filter sdm845 sdm710 sdm660 msm8953 msm8998, $(TARGET_BOARD_PLATFORM)))
-    BOARD_SEPOLICY_DIRS := \
-                 $(BOARD_SEPOLICY_DIRS) \
-                 $(SEPOLICY_PATH) \
-                 $(SEPOLICY_PATH)/legacy/vendor/common/sysmonapp \
-                 $(SEPOLICY_PATH)/legacy/vendor/ssg \
-                 $(SEPOLICY_PATH)/legacy/vendor/common
-
-    ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/$(TARGET_BOARD_PLATFORM)
-    else
-      BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/$(TARGET_SEPOLICY_DIR)
-    endif
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/test
-    endif
-endif
+  ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+  BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/test
+  BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test
+  BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test/sysmonapp
+  endif
 endif
